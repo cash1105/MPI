1d7c3cd60b9b98002daacce7a2f22a1ba9b92eae
cash1105 <cash1105@gmail.com>
2016-11-21 23:03:13 -0500

    Update csalazar-02.c

--- csalazar-02.c
+++ csalazar-02.c
@@ -10,15 +10,18 @@ int main(int argc, char **argv) {
 	char de_msg[100];
 	char de_buf[20];
 	MPI_Status status;
-
+	
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &p);
 	if (my_rank == MASTER) {
 		strcpy(or_msg, "1");
-		MPI_Send(or_msg, strlen(or_msg) + 1, MPI_CHAR, 1, tag, MPI_COMM_WORLD);
+		// Implementacion protegida de deadlock
+	        MPI_Sendrecv(or_msg,strlen(or_msg) + 1, MPI_CHAR, 1, tag,or_buf, 20, MPI_CHAR, 1, tag, MPI_COMM_WORLD,&status);
+		// Implementacion basica
+		//MPI_Send(or_msg, strlen(or_msg) + 1, MPI_CHAR, 1, tag, MPI_COMM_WORLD);
 		printf("El mensaje %s ha sido enviado\n",or_msg);
-		MPI_Recv(or_buf, 20, MPI_CHAR, 1, tag, MPI_COMM_WORLD, &status);
+		//MPI_Recv(or_buf, 20, MPI_CHAR, 1, tag, MPI_COMM_WORLD, &status);
 		printf("El mensaje modificado es: %s\n",or_buf);
 	} else if (my_rank == 1) {
 		MPI_Recv(de_buf, 20, MPI_CHAR, 0, tag, MPI_COMM_WORLD, &status);
